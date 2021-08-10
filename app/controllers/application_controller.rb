9b7f71af7a12b410535e4c03a9b652637fb6ab1e
CutyDog <silver.soul.818@gmail.com>
2021-08-10 21:55:59 +0900

    .env削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::API
 
+
     def hello
         client = Slack::Web::Client.new
-        client.chat_postMessage(channel: '#botテスト', text: 'レムちゃんだよ♪')
+        client.chat_postMessage(channel: '#botテスト', text: 'おはようございます♪')
         # client.chat_postMessage(channel: '#general', text: 'Hello World', as_user: true)
+    end
+
+    def name
+        client = Slack::Web::Client.new
+        client.chat_postMessage(channel: '#botテスト', text: 'スバル君のレムです')
+    end
+
+    def love
+        client = Slack::Web::Client.new
+        client.chat_postMessage(channel: '#botテスト', text: 'レムは、スバル君を愛しています')
     end
 end
