005750f755ebc28d18966b7d51bdf26e57d40db3
CutyDog <silver.soul.818@gmail.com>
2021-08-11 07:25:55 +0900

    Real Time Message ver.5

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -1,21 +1,18 @@
 module  Body
+
     class TestService
         def initialize(json)
             @json=json
         end
         def execute
-        #Faradayを使って、JSON形式のファイルをPOSTできるようにする
-                conn = Faraday::Connection.new(:url => 'https://my-men.slack.com') do |builder|
+            #Faradayを使って、JSON形式のファイルをPOSTできるようにする
+            conn = Faraday::Connection.new(:url => 'https://.slack.com') do |builder|
                 builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
                 builder.use Faraday::Response::Logger     # リクエストを標準出力に出力する
-                builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
-                end
+                builder.adapter Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
+            end
             if @json[:event][:subtype] != "bot_message" #これがないと無限ループになる
-            body = {
-                        :token => ENV['BOT_USER_ACCESS_TOKEN'],#あとでherokuで設定します
-                        :channel => '#botテスト',#こうするとDM内に返信できます
-                        :text  => "こんにちは、私はslackbotです！"
-                    }
+            body = {token: ENV['BOT_USER_ACCESS_TOKEN'], channel: '#botテスト', text: "こんにちは、私はslackbotです！"}
             conn.post '/api/chat.postMessage',body.to_json,
                 {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['BOT_USER_ACCESS_TOKEN']}"}
                 #ヘッダーはつけなければいけないらしい、このままで大丈夫です。
